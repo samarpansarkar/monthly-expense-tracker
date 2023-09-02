53c83333709095d0fda74b26a12bbfd4cb36b672
samarpan sarkar <samarpansarkar209@gmail.com>
2023-09-02 22:55:07 +0530

    v-82

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import ExpenseItem from "./Components/ExpenseItem/ExpenseItem";
 import Card from "./Components/Card/Card";
 import NewExpense from "./Components/NewExpense/NewExpense";
+import ExpenseFilter from "./Components/ExpenseFilter/ExpenseFilter";
 
-function App() {
+function App(props) {
   const expense = [
     {
       id: "01",
@@ -33,11 +34,41 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expenses) => {
+    console.log(expenses);
+    console.log("In App.js");
+  };
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  let filterInfoText = "2019,2021 & 2022";
+
+  if (filteredYear === "2019") {
+    filterInfoText = "2020,2021 & 2022";
+  } else if (filteredYear === "2020") {
+    filterInfoText = "2019,2021 & 2022";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019,2020 & 2022";
+  } else {
+    filterInfoText = "2019,2020 & 2021";
+  }
+
+  const addExpenseFilter = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <div className="App">
       <Card className="appContainer">
         <Navbar />
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseHandler} />
+        <ExpenseFilter
+          defaultYear={filteredYear}
+          onChangeFilter={addExpenseFilter}
+        />
+        <p className="filterInfo-text">
+          <h3>Note: {filterInfoText} year data are hidden!</h3>
+        </p>
         <ExpenseItem Expense={expense[0]} />
         <ExpenseItem Expense={expense[1]} />
         <ExpenseItem Expense={expense[2]} />
